f2062b4d2044a12007474630c5f00edb82098eb6
Riyaad_Hossain <riyadhossain.dev@gmail.com>
2022-05-18 18:49:44 +0600

    home and log in Page done

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,11 +1,20 @@
-import React from 'react';
+import React from "react";
+import { Link } from "react-router-dom";
 
 const Home = () => {
-    return (
-        <div>
-            
+  return (
+    <div class="hero min-h-screen bg-base-200">
+      <div class="hero-content text-center">
+        <div class="max-w-md">
+          <h1 class="text-5xl font-bold">To Do App</h1>
+          <p class="py-6">
+          Writing your daily tasks on a list can help you increase productivity and decrease stress. To-do lists enable you to segment your goals into achievable activities and complete large projects by breaking them into smaller assignments.
+          </p>
+          <Link to='/addtask' class="btn btn-primary">Add Task</Link>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default Home;
--- src/Components/LogIn/LogIn.jsx
+++ src/Components/LogIn/LogIn.jsx
@@ -1,11 +1,40 @@
-import React from 'react';
+import React from "react";
 
 const LogIn = () => {
-    return (
-        <div>
-            
+  return (
+    <div class="card h-screen flex items-center justify-center bg-base-100 shadow-xl">
+        <h1 className="text-center text-5xl text-primary font-bold mb-12">Please Log In</h1>
+      <div class="rounded-lg py-5 shadow-2xl w-96 bg-base-300">
+        <div class="card-body">
+          <div class="form-control">
+            <label class="label">
+              <span class="label-text">Email</span>
+            </label>
+            <input
+              type="text"
+              placeholder="email"
+              class="input input-bordered"
+            />
+          </div>
+          <div class="form-control">
+            <label class="label">
+              <span class="label-text">Password</span>
+            </label>
+            <input
+              type="text"
+              placeholder="password"
+              class="input input-bordered"
+            />
+          </div>
+          <div class="form-control mt-6">
+            <button class="btn btn-primary">Login</button>
+          </div>
+          <div class="divider">OR</div>
+          <button className="btn w-full btn-accent">Continue With Google</button>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default LogIn;
